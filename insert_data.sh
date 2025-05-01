56d5757f2236c386b6a5f6d64c1a87b071743c1a
amjadsh92 <157421106+amjadsh92@users.noreply.github.com>
2025-05-01 17:27:38 +0300

    Update insert_data.sh
    local machine version

--- insert_data.sh
+++ insert_data.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-if [[ $1 == "test" ]]
-then
-  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
-else
-  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
-fi
+# if [[ $1 == "test" ]]
+# then
+#   PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+# else
+#   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+# fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
 
-
+PSQL="psql --username=amjad --dbname=worldcup -t --no-align -c"
 echo $($PSQL "TRUNCATE games,teams RESTART IDENTITY")
 CONTAINS_ELEMENT() {
   elem="$1"
@@ -22,33 +22,34 @@ CONTAINS_ELEMENT() {
   return 1
 }
 
- TEAMS=()
+TEAMS=()
+echo it is working
  while IFS="," read _ _ WINNER OPPONENT _ _; 
   do
   
   if ! CONTAINS_ELEMENT "$WINNER" "${TEAMS[@]}";
   then
-    TEAMS+=("$WINNER")
-    INSERT_TEAM=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')" );
-  if [[ $INSERT_TEAM == "INSERT 0 1" ]]
-  then
-    echo Inserted into teams, $WINNER
-  fi
+  TEAMS+=("$WINNER")
+  
   fi
   if ! CONTAINS_ELEMENT "$OPPONENT" "${TEAMS[@]}" ;
   then
   TEAMS+=("$OPPONENT")
-  INSERT_TEAM=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')" );
-  if [[ $INSERT_TEAM == "INSERT 0 1" ]]
-  then
-    echo Inserted into teams, $OPPONENT
-  fi
+  
   fi
 
 
 done < <(tail -n +2 games.csv)
 
+for TEAM in "${TEAMS[@]}";
+do
 
+INSERT_TEAM=$($PSQL "INSERT INTO teams(name) VALUES('$TEAM')" );
+if [[ $INSERT_TEAM == "INSERT 0 1" ]]
+then
+  echo Inserted into teams, $TEAM
+fi
+done
 
 tail -n +2 games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS;
 do
@@ -60,3 +61,69 @@ INSERT_GAME=$($PSQL "INSERT INTO games(year,round,winner_id,opponent_id,winner_g
 echo Inserted into games, game_info:"$YEAR","$ROUND","$WINNER","$OPPONENT","$WINNER_GOALS","$OPPONENT_GOALS"
 fi
 done
+
+
+
+
+# #! /bin/bash
+#cloud version
+# if [[ $1 == "test" ]]
+# then
+#   PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+# else
+#   PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+# fi
+
+# # Do not change code above this line. Use the PSQL variable above to query your database.
+
+
+# echo $($PSQL "TRUNCATE games,teams RESTART IDENTITY")
+# CONTAINS_ELEMENT() {
+#   elem="$1"
+#   shift
+#   arr=("$@")  # Grab the rest of the arguments as an array
+
+#   for i in "${arr[@]}"; do
+#     [[ "$i" == "$elem" ]] && return 0
+#   done
+#   return 1
+# }
+
+#  TEAMS=()
+#  while IFS="," read _ _ WINNER OPPONENT _ _; 
+#   do
+  
+#   if ! CONTAINS_ELEMENT "$WINNER" "${TEAMS[@]}";
+#   then
+#     TEAMS+=("$WINNER")
+#     INSERT_TEAM=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')" );
+#   if [[ $INSERT_TEAM == "INSERT 0 1" ]]
+#   then
+#     echo Inserted into teams, $WINNER
+#   fi
+#   fi
+#   if ! CONTAINS_ELEMENT "$OPPONENT" "${TEAMS[@]}" ;
+#   then
+#   TEAMS+=("$OPPONENT")
+#   INSERT_TEAM=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')" );
+#   if [[ $INSERT_TEAM == "INSERT 0 1" ]]
+#   then
+#     echo Inserted into teams, $OPPONENT
+#   fi
+#   fi
+
+
+# done < <(tail -n +2 games.csv)
+
+
+
+# tail -n +2 games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS;
+# do
+# WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+# OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+# if [[ -n "$WINNER_ID" && -n "$OPPONENT_ID" ]];
+# then
+# INSERT_GAME=$($PSQL "INSERT INTO games(year,round,winner_id,opponent_id,winner_goals,opponent_goals) VALUES('$YEAR','$ROUND','$WINNER_ID','$OPPONENT_ID','$WINNER_GOALS','$OPPONENT_GOALS')") 
+# echo Inserted into games, game_info:"$YEAR","$ROUND","$WINNER","$OPPONENT","$WINNER_GOALS","$OPPONENT_GOALS"
+# fi
+# done
