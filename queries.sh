79bec94ea2713fb8bbf6deaf3266b15508e933bf
amjadsh92 <157421106+amjadsh92@users.noreply.github.com>
2025-05-01 17:30:08 +0300

    Update queries.sh
    local machine version

--- queries.sh
+++ queries.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=worldcup --no-align --tuples-only -c"
+# PSQL="psql --username=freecodecamp --dbname=worldcup --no-align --tuples-only -c"
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+
+PSQL="psql --username=amjad --dbname=worldcup --no-align --tuples-only -c"
 
 echo -e "\nTotal number of goals in all games from winning teams:"
 echo "$($PSQL "SELECT SUM(winner_goals) FROM games")"
